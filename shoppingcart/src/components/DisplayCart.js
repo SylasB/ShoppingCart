88ae3afb885ea6f502b90256b62e956bc8209544
SylasB <sylasboniek@gmail.com>
2023-02-28 06:22:15 -0700

    Add the functionality for the count system for each item. Also added some minor styling.

---
+++ shoppingcart/src/components/DisplayCart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DisplayCart() {
+    return (
+        <>
+        <p>Items In Cart Go Here</p>
+        </>
+    )
+}
+
+export default DisplayCart;
--- shoppingcart/src/components/Item.js
+++ shoppingcart/src/components/Item.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item(props) {
     const { name, id } = props;
+    const [ count, setCount ] = useState(0);
+
+    const handleAddCount = () => {
+        if (count == 99) {
+            return
+        }
+        setCount(count + 1);
+    }
+
+    const handleLowerCount = () => {
+        if (count == 0) {
+            return
+        }
+        setCount(count - 1);
+    }
+
     return (
         <div className="itemInnerContainer">
             <div className="itemIMG">
                 <p>IMG here</p>
             </div>
-            <div>
+            <div className="itemDescription">
                 {name} {id}
-                <p>---------------------</p>
-            </div>    
+            </div>
+            <div className="quantityContainer">
+                <div className="lowerCount" onClick={handleLowerCount}>-</div><div className="count">{count}</div><div className="addCount" onClick={handleAddCount}>+</div>
+            </div>
+            <div className="buttonContainer">
+                <button type="button" className="addToCartButton">Add To Cart</button>
+            </div>
         </div>
     )
 }
